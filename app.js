7adba14c5b47221c1301ae0b7467ecd5c0a8bf6c
Anniel <annielfranciscoreyes@gmail.com>
2023-01-07 19:34:53 -0400

    New

---
+++ app.js
@@ -0,0 +1,7 @@
+let contacto = document.querySelector(".contacto");
+let correo = document.querySelector(".correo");
+
+contacto.onclick = () => {
+  correo.style.visibility = "visible";
+  setTimeout(() => (correo.style.visibility = "hidden"), 5000);
+};
