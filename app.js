09b8ce307fb85783e9ced3c183a1d8e674ca62c6
Anniel <annielfranciscoreyes@gmail.com>
2023-01-08 00:04:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 let contacto = document.querySelector(".contacto");
 let correo = document.querySelector(".correo");
 
-contacto.onclick = () => {
-  correo.style.visibility = "visible";
-  setTimeout(() => (correo.style.visibility = "hidden"), 5000);
-};
+contacto.addEventListener("click", () => {
+  Notification.requestPermission().then((resultado) => console.log(resultado));
+
+  if (Notification.permission === "granted") {
+    new Notification("annielfranciscoreyes@gmail.com");
+  }
+});
+
+// contacto.onclick = () => {
+//   correo.style.visibility = "visible";
+//   setTimeout(() => (correo.style.visibility = "hidden"), 5000);
+// };
