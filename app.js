1a5f0de743733773b1d806ce63617950b22309fc
Anniel <annielfranciscoreyes@gmail.com>
2023-01-10 17:00:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,22 +3,8 @@ let proyectos = document.querySelector(".proyectos");
 let habilidades = document.querySelector(".habilidades");
 
 //Eventos
-
-window.addEventListener("online", actualizarEstado);
-window.addEventListener("offline", actualizarEstado);
-
-function actualizarEstado() {
-  if (navigator.onLine) {
-    console.log("Conectado");
-  } else {
-    let conex = document.createElement("h1");
-    conex.textContent = "Sin Conexión...";
-    conex.style.textAlign = "center";
-    document.write(conex);
-  }
-}
-
 eventListener();
+
 function eventListener() {
   contacto.addEventListener("click", () => {
     Notification.requestPermission().then((resultado) =>
